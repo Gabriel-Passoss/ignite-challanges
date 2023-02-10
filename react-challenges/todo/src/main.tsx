e8707fa519ee6cab56eb33c80976a467142f9364
Gabriel dos Passos <gabrielp@192.168.100.15>
2023-02-09 21:50:59 -0300

    Added the global styles and theme

--- react-challenges/todo/src/main.tsx
+++ react-challenges/todo/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { App } from './App'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
